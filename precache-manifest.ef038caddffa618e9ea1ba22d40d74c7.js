10b7880e2b839d006daa016d0f35e9a0a24345f5
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2020-08-20 18:45:55 +0530

    Updates

---
+++ precache-manifest.ef038caddffa618e9ea1ba22d40d74c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab0a7ab37e8a35cbe37a36e233fa0dfd",
+    "url": "/smart_brain/index.html"
+  },
+  {
+    "revision": "b6cf67fef84e138950a5",
+    "url": "/smart_brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "858873ab6dafcfd8a0c3",
+    "url": "/smart_brain/static/css/main.0a75c7fd.chunk.css"
+  },
+  {
+    "revision": "b6cf67fef84e138950a5",
+    "url": "/smart_brain/static/js/2.365f411f.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smart_brain/static/js/2.365f411f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "858873ab6dafcfd8a0c3",
+    "url": "/smart_brain/static/js/main.ec99ed65.chunk.js"
+  },
+  {
+    "revision": "3619ba69d0cedf31a22a",
+    "url": "/smart_brain/static/js/runtime-main.3f2c599b.js"
+  }
+]);
